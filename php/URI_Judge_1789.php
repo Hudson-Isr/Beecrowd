a628f24e10118a0a6f3b19499f20728e8659403d
Hudson Israel <hudson.israel716@gmail.com>
2023-03-15 14:31:55 -0300

    A Corrida de Lesmas
    Realizado ajustes para o beecrowd aceitar o desafio.

--- php/URI_Judge_1789.php
+++ php/URI_Judge_1789.php
@@ -1,21 +1,28 @@
 <?php
-    $total_lesmas = readline();
-    $s = 0;
 
-    $t = readline();
-    $t = explode(" ",$t);
-
-    for($i = 0; $i <= $total_lesmas; $i++){           
-        if($s<$t[$i]){
-            $s = $t[$i];
+    while(1){   
+    
+        $total_lesmas = readline();
+        $s = 0;
+    
+        if ($total_lesmas == "" || $total_lesmas == " "){
+            break;
+        } 
+        $t = readline();
+        $t = explode(" ",$t);
+    
+        for($i = 0; $i <= $total_lesmas; $i++){           
+            if($s<$t[$i]){
+                $s = $t[$i];
+            }
         }
-    }
-    if ($s >= 20){
+        if ($s >= 20){
         echo "3\n";
-    } 
-    elseif ($s >= 10 && $s < 20){
+        } 
+        elseif ($s >= 10 && $s < 20){
         echo "2\n";
-    } 
-    else {echo "1\n";}
+        } 
+        else {echo "1\n";}
+}
 
 ?>
